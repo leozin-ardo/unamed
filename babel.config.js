2faa62bf55b5d57ecf40a5f1905dc58cf63fd26c
Leonardo A R De Oliveira <leonardo.oliveira@negocioanimal.com.br>
2023-02-27 12:27:53 -0300

    feat(config dinamic paths): add babel pluigns and create paths in tsconfig and babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ['./src/'],
+      alias: {
+        components: './src/view/components/',
+        screens: './src/view/screens/',
+      },
+    },
+  ],
+];
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins,
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../view/Home';
+import Home from 'screens/Home';
 
 const Stack = createStackNavigator();
 
